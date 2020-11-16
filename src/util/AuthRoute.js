44cc62ef2cbef5892796bc68e5df433539a5ab20
Ruvie Eto <ruvie.eto@gmail.com>
2020-11-15 21:56:20 -0700

    auth route redirecting

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,1 +1,15 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const AuthRoute = ({ component: Component, authenticated, ...rest }) =>{
+	return(
+		<Fragment>
+			<Route 
+			{...rest} 
+			render={(props) => authenticated === true ? <Redirect to="/admin/dashboard" /> : <Component {...props} />}
+			/>
+		</Fragment>
+		)
+}
+
+export default AuthRoute;
